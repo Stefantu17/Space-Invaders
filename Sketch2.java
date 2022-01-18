16f90f46b20ed280a8086d0b07727f1fc86e76e8
John MacDonald <john.macdonald23@ycdsbk12.ca>
2022-01-18 13:54:51 -0500

    Main Menu (SERIOUS work in progress)

--- Sketch2.java
+++ Sketch2.java
@@ -2,59 +2,33 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Sketch2 extends PApplet {
-PImage Background;
-PImage Settings; 
-
-  boolean background = true;
-  
+  PImage Background;
+  PImage Settings; 
 
   public void settings() {
-	  // put your size call here
     size(1200, 900);
   }
 
-  
   public void setup(){
-    
+   Background = loadImage("../assets/CPTBackground.jpg");
+   Settings = loadImage("../assets/126472.png");
+
   }
   
   public void draw() {   
-    size(1200,900);
-    Background = loadImage("../assets/CPTBackground.jpg");
-    
-    background(0);
     image(Background, 0, 0);
+    
+    fill(30, 166, 37);
+    rect(800,600,300,200); 
+    image(Settings, 600, 450); 
     
     fill(38, 38, 36);
     ellipse(250,250,750,750);
-    
     
     fill(30, 166, 37);
     rect(800,100,300,200);
     
     fill(30, 166, 37);
     rect(800,350,300,200);
-    
-    // settings button
-    size(300,300);
-    Settings = loadImage("../assets/126472.png");
-    
-
-    fill(30, 166, 37);
-    rect(800,600,300,200);
-    
   }
-
-boolean isMouseOver(int x, int y, int w, int h){
-  if(mouseX >= x && mouseX <= x + w && mouseY >= y && mouseY <= y +h){
-    return true;
-  }
-  return false;
-}
-
-public void mousePressed(){
-  if(isMouseOver(100,100,400,400) == true){
-    background = !background;
-  }
-}
 }
