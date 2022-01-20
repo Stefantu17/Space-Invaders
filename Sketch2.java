e3928d7d268a4a698437af5f72520eee1ed82792
John MacDonald <john.macdonald23@ycdsbk12.ca>
2022-01-20 14:13:29 -0500

    menu final design

--- Sketch2.java
+++ Sketch2.java
@@ -5,6 +5,9 @@ public class Sketch2 extends PApplet {
   PImage background;
   PImage settings;
   PImage play; 
+  PImage quit;
+  PImage spaceinvasion;
+  PImage credits;
 
   public void settings() {
     size(600, 600);
@@ -17,10 +20,17 @@ public class Sketch2 extends PApplet {
   settings.resize(75, 75);
   play = loadImage("../assets/playbutton.png");
   play.resize(75, 75);
+  quit = loadImage("../assets/powerbutton.png");
+  quit.resize(75, 75);
+  spaceinvasion = loadImage("../assets/spaceinvasion.png");
+  spaceinvasion.resize(200, 150);
+  credits = loadImage("../assets/credits.png");
+  credits.resize(500, 25);
   }
   
   public void draw() {   
     image(background, 0, 0);
+    image(credits, 5, 550);
     
     fill(255, 255, 255);
     rect(325,150,200,100); 
@@ -28,12 +38,14 @@ public class Sketch2 extends PApplet {
 
     fill(255, 255, 255);
     ellipse(100,100,350,350);
-    
+    image(spaceinvasion, 25, 50);
+
     fill(255, 255, 255);
     rect(325,275,200,100);
     image(settings, 387, 286); 
     
     fill(255, 255, 255);
     rect(325,400,200,100);
+    image(quit, 387, 415);
   }
 }
