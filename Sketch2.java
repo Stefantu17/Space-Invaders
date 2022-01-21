1de1e045707ef0061f69078266a7c540ec9ffceb
John MacDonald <john.macdonald23@ycdsbk12.ca>
2022-01-21 14:20:15 -0500

    menu buttons updated

--- Sketch2.java
+++ Sketch2.java
@@ -2,6 +2,7 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Sketch2 extends PApplet {
+  // setting image variables
   PImage background;
   PImage settings;
   PImage play; 
@@ -9,11 +10,17 @@ public class Sketch2 extends PApplet {
   PImage spaceinvasion;
   PImage credits;
 
+  // rectangle variables
+  public boolean onPlay = false;
+  public boolean onSettings = false;
+  public boolean onExit = false;
+  
   public void settings() {
     size(600, 600);
   }
 
   public void setup(){
+    // loading and resizing images
     background = loadImage("../assets/background.jpg");
     background.resize(600, 600);
     settings = loadImage("../assets/settings.png");
@@ -28,24 +35,46 @@ public class Sketch2 extends PApplet {
     credits.resize(500, 25);
   }
   
-  public void draw() {   
+  public void draw() {  
+    // background and credits 
     image(background, 0, 0);
     image(credits, 5, 550);
     
+    // play button
     fill(255, 255, 255);
     rect(325,150,200,100); 
     image(play, 390, 165);
 
+    // game logo
     fill(255, 255, 255);
     ellipse(100,100,350,350);
     image(spaceinvasion, 25, 50);
 
+    // settings button
     fill(255, 255, 255);
     rect(325,275,200,100);
     image(settings, 387, 286); 
     
+    // exit button
     fill(255, 255, 255);
     rect(325,400,200,100);
     image(quit, 387, 415);
+  }
+
+  public void mousePressed() {
+    // play button press
+    if ((mouseX >= 325 && mouseX <= 200) && (mouseY >= 150 && mouseY <= 100)){
+      onPlay = true;
+    }
+
+    // settings button press
+    if ((mouseX >= 325 && mouseX <= 200) && (mouseY >= 275 && mouseY <= 100)){
+      onSettings = true;
+    }
+
+    // exit button press
+    if ((mouseX >= 325 && mouseX <= 200) && (mouseY >= 400 && mouseY <= 100)){
+      onExit = true;
+    }
   }
 }
