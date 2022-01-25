d4c8514a47d245a5fbd8e2e22a6f873bee29fbc2
John MacDonald <john.macdonald23@ycdsbk12.ca>
2022-01-25 12:25:11 -0500

    settings button complete

--- Sketch2.java
+++ Sketch2.java
@@ -9,6 +9,7 @@ public class Sketch2 extends PApplet {
   PImage quit;
   PImage spaceinvasion;
   PImage credits;
+  PImage settingsexit;
 
   // rectangle variables
   public boolean onPlay = false;
@@ -33,10 +34,18 @@ public class Sketch2 extends PApplet {
     spaceinvasion.resize(200, 150);
     credits = loadImage("../assets/credits.png");
     credits.resize(500, 25);
+    settingsexit = loadImage("../assets/settingsexit.png");
+    settingsexit.resize(50,50);
   }
   
   public void draw() {  
     // background and credits 
+
+    background(87);
+
+    fill(255, 255, 255);
+    rect(110, 150, 200, 100);
+
     image(background, 0, 0);
     image(credits, 5, 550);
     
@@ -59,6 +68,8 @@ public class Sketch2 extends PApplet {
     fill(255, 255, 255);
     rect(325,400,200,100);
     image(quit, 387, 415);
+
+    
   }
 
   public void mousePressed() {
@@ -68,13 +79,69 @@ public class Sketch2 extends PApplet {
     }
 
     // settings button press
-    if ((mouseX >= 325 && mouseX <= 200) && (mouseY >= 275 && mouseY <= 100)){
-      onSettings = true;
-    }
+    if ((mouseX >= 325 && mouseX <= 525) && (mouseY >= 275 && mouseY <= 375)){
+      background(87, 87, 87);
 
-    // exit button press
+      fill(255, 255, 255);
+      rect(25, 500, 125, 75);
+      image(settingsexit, 60, 515);
+      
+      fill(255, 255, 255);
+      rect(75, 75, 200, 50);
+
+      fill(255, 255, 255);
+      rect(75, 175, 200, 50);
+
+      fill(255, 255, 255);
+      rect(75, 275, 200, 50);
+
+      fill(255, 255, 255);
+      rect(75, 375, 200, 50);
+
+      redraw();
+      noLoop();
+    }
+    
+      // exit button press
     if ((mouseX >= 325 && mouseX <= 525) && (mouseY >= 400 && mouseY <= 500)){
       exit();
+
+    }
+    // settingsexit button press
+    if ((mouseX >= 25 && mouseX <= 150) && (mouseY >= 500 && mouseY <= 575)){
+    background(87);
+
+    fill(255, 255, 255);
+    rect(110, 150, 200, 100);
+
+    image(background, 0, 0);
+    image(credits, 5, 550);
+    
+    // play button
+    fill(255, 255, 255);
+    rect(325,150,200,100); 
+    image(play, 390, 165);
+
+    // game logo
+    fill(255, 255, 255);
+    ellipse(100,100,350,350);
+    image(spaceinvasion, 25, 50);
+
+    // settings button
+    fill(255, 255, 255);
+    rect(325,275,200,100);
+    image(settings, 387, 286); 
+    
+    // exit button
+    fill(255, 255, 255);
+    rect(325,400,200,100);
+    image(quit, 387, 415);
+      
+    redraw();
+    loop();
+
+    
+    
     }
   }
 }
